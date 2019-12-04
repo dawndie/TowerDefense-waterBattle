ddc0a646b39c5d875b2d5e808510a740cc265070
dawndie <53926858+dawndie@users.noreply.github.com>
2019-12-04 17:58:33 +0700

    Update NormalEnemy.java

--- src/towerDefense/gameEntity/enemy/NormalEnemy.java
+++ src/towerDefense/gameEntity/enemy/NormalEnemy.java
@@ -5,7 +5,7 @@ import towerdefense.Player;
 
 public class NormalEnemy extends towerdefense.gameEntity.enemy.Enemy {
     public NormalEnemy(Player player, GameField gameField) {
-        super(150, 20, 1f, 15, 25, "res\\GameEntity\\Enemy\\NormalEnemy.png");
+  
         this.player=player;
         this.gameField=gameField;
     }
