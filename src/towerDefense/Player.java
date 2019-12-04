3aff602b27616d838beffe0f92e57743d2a5f0a2
dawndie <53926858+dawndie@users.noreply.github.com>
2019-12-04 18:02:46 +0700

    Update Player.java

--- src/towerDefense/Player.java
+++ src/towerDefense/Player.java
@@ -6,8 +6,8 @@ public class Player {
     public int health;
 
     public Player() {
-        this.health = towerdefense.User.DEFAULT_HEALTH;
-        this.coin = towerdefense.User.DEFAULT_COIN;
+        this.health =100;
+        this.coin =300;
 
     }
 }
